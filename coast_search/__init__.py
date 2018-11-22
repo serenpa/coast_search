ff330c22b77278b22903cd93270b022e9ab1561a
Ashley James Williams <awi111@cs15078kn.canterbury.ac.nz>
2018-11-22 14:25:20 +1300

    Added versioneer and init file

---
+++ coast_search/__init__.py
@@ -0,0 +1,16 @@
+"""
+    Title: coast_search
+
+    Author: Ashley Williams
+
+    Description: COAST_SEARCH allows you to generate and run queries against the Google Custom Search API.
+"""
+from __future__ import print_function
+from ._version import get_versions
+
+
+
+__author__ = 'Ashley Williams'
+__email__ = 'ashley.williams@pg.canterbury.ac.nz'
+__version__ = get_versions()['version']
+del get_versions
