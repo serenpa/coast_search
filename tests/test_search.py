2d0c6a2e88666a2bc7adc2ff3057c4b041e3be08
emr65 <emr65@uclive.ac.nz>
2018-12-06 15:25:43 +1300

    deduplication function working to some extent, will check it with Ash tomorrow

--- tests/test_search.py
+++ tests/test_search.py
@@ -15,21 +15,11 @@ from coast_search import utils
 
 class TestSearch(unittest.TestCase):
 
-    # def setUp(self):
-        # self.dimensions_dict = {
-        #     "reasoning": ['because', 'however', 'conclude', 'but', 'for example'],
-        #     "experience": ['i', 'me', 'our', 'we', 'in my experience'],
-        #     "topic": ['trustworthy', 'software']
-        # }
 
     def test_write_to_file_json(self):
 
         test_dict = {1: 'a', 2: 'b', 'c': 3}
         search.write_to_file("test_file", test_dict, "/Users/liz/Local Documents/Work/coast_search/tests/test_output/", ".json")
-
-        json_data = utils.get_json_from_file("/Users/liz/Local Documents/Work/coast_search/tests/test_output/test_file")
-
-        self.assertEqual(test_dict, json_data)
 
 
     def test_write_to_file_txt(self):
@@ -44,7 +34,48 @@ class TestSearch(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
-    def test_deduplicate_urls(self):
-        filename = ""
+    def test_deduplicate_urls_no_repetiton_between_segments(self):
+        filename = "/Users/liz/Local/coast_search/tests/test_data/results_for_testing_dedup.json"
+        deduplicated_urls_object = search.deduplicate_urls(filename)
+        deduplicated_urls_list = deduplicated_urls_object["deduplicated_urls"]
+
+        expected = [
+            "https://medium.com/s/story/whats-wrong-with-software-development-language-cart-before-the-engineering-horse-c477df24e94d",
+            "https://www.msnbc.com/msnbc/watch/apple-ceo-tim-cook-learning-to-code-is-important-because-software-touches-everything-we-do-1204860483791",
+            "https://writingcooperative.com/im-a-software-engineer-not-a-writer-but-i-want-to-write-6fa9af8290b6",
+            "https://www.eetimes.com/author.asp?section_id=36&doc_id=1329106",
+            "https://www.because-software.com/",
+            "https://en.wikipedia.org/wiki/Therac-25",
+            "https://kb.iu.edu/d/afdk",
+            "https://en.wikipedia.org/wiki/Freeware"
+        ]
+        expected.sort()
+        deduplicated_urls_list.sort()
+
+        self.assertEqual(expected, deduplicated_urls_list)
+        self.assertRaises(KeyError, lambda: deduplicated_urls_object["warning"])
+
+    def test_deduplicate_urls_repetiton_between_segments(self):
+        filename = "/Users/liz/Local/coast_search/tests/test_data/results_for_testing_dedup_between_seg.json"
+
+        deduplicated_urls_object = search.deduplicate_urls(filename)
+        deduplicated_urls_list = deduplicated_urls_object["deduplicated_urls"]
+
+        expected = [
+            "https://medium.com/s/story/whats-wrong-with-software-development-language-cart-before-the-engineering-horse-c477df24e94d",
+            "https://www.msnbc.com/msnbc/watch/apple-ceo-tim-cook-learning-to-code-is-important-because-software-touches-everything-we-do-1204860483791",
+            "https://writingcooperative.com/im-a-software-engineer-not-a-writer-but-i-want-to-write-6fa9af8290b6",
+            "https://www.eetimes.com/author.asp?section_id=36&doc_id=1329106",
+            "https://www.because-software.com/",
+            "https://en.wikipedia.org/wiki/Therac-25",
+            "https://kb.iu.edu/d/afdk",
+            "https://kb.iu.edu/d/afdk",
+            "https://en.wikipedia.org/wiki/Freeware"
+        ]
+        expected.sort()
+        deduplicated_urls_list.sort()
+
+        self.assertEqual(expected, deduplicated_urls_list)
+        self.assertEqual("same url found across more than 1 segment", deduplicated_urls_object["warning"])
 
 
